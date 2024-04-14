ac90465e2c96d4855648670166b6f1f9161dbabc
Mateus_Maffi <121996649+MMaffi@users.noreply.github.com>
2024-04-14 18:14:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const gato = document.querySelector('.gato');
+const obstaculo = document.querySelector('.obstaculo');
+
+const jump = () => {
+
+    gato.classList.add('jump');
+
+    setTimeout(() => {
+
+        gato.classList.remove('jump');
+
+    }, 500);
+}
+
+const loop = setInterval(() => {
+
+    const obstaculoposicao = obstaculo.offsetLeft;
+    const gatoposicao = +window.getComputedStyle(gato).bottom.replace('px', '');
+
+    if(obstaculoposicao <= 155 && obstaculoposicao > 0 && gatoposicao < 50) {
+
+        obstaculo.style.animation = 'none';
+        obstaculo.style.left = `${obstaculoposicao}px`;
+
+        gato.style.animation = 'none';
+        gato.style.bottom = `${gatoposicao}px`;
+
+        // gato.src = 'images/game-over.png'
+
+        clearInterval(loop);
+
+    }
+
+}, 10);
+
+document.addEventListener('keydown', jump);
